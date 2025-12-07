21d638717d29999ce6415d8890d646ff8d08985f
Muthoni(sonnieperis) <sonnieperis17@gmail.com>
2025-12-07 22:31:48 +0300

    updated movie app

--- alx-movie-app/components/layouts/Header.tsx
+++ alx-movie-app/components/layouts/Header.tsx
@@ -8,20 +8,23 @@ const Header: React.FC = () => {
         <h2 className="text-xl md:text-4xl font-semibold">
           Cine<span className="text-[#E2D609]">Seek</span>
         </h2>
+
         <nav className="hidden md:flex flex-1 justify-center space-x-8">
           <Link href="/" className="hover:text-[#E2D609] px-4 md:px-8 text-xl transition-colors duration-300 font-semibold">Home</Link>
           <Link href="/movies" className="hover:text-[#E2D609] px-4 md:px-8 text-xl transition-colors duration-300 font-semibold">Movies</Link>
           <Link href="/contact" className="hover:text-[#E2D609] px-4 md:px-8 text-xl transition-colors duration-300 font-semibold">Contact</Link>
         </nav>
+
         <div className="flex md:hidden">
           <Button title="Sign in" />
         </div>
+
         <div className="hidden md:flex">
           <Button title="Sign in" />
         </div>
       </div>
     </header>
-  )
-}
+  );
+};
 
 export default Header;
